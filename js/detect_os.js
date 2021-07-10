4d08a34ed95405db9ea8cd1bdecff2cdff0958ea
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2021-07-11 02:53:21 +0430

    Add Download Button and Release Notes link
    + And some header menu changes!
    
    There are problems with the size of the button and link on the small screens and I would be happy if someone could help to fix them!

---
+++ js/detect_os.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+    var button = document.getElementById("download_button");
+    var os_name = "Download";
+    var os_link = "download/official_releases.html"
+
+    if (navigator.userAgent.indexOf("Win") != -1) os_name =
+        "Download For Windows", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-win.zip";
+    if (navigator.userAgent.indexOf("Mac") != -1) os_name =
+        "Download For macOS", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-osx.dmg";
+    if (navigator.userAgent.indexOf("Linux") != -1) os_name =
+        "Download For Linux", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-linux.tar.gz";
+
+    button.text = os_name;
+    button.href = os_link;
+
+}
