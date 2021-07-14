2af1e33e604f923234c1b5fc3d80f973133dbdd6
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2021-07-14 05:57:36 +0430

    Add AOS library and fix the problem of detecting Android and iOS operating systems

--- js/detect_os.js
+++ js/detect_os.js
@@ -11,6 +11,11 @@ $(function() {
     if (navigator.userAgent.indexOf("Linux") != -1) os_name =
         "Download For Linux", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-linux.tar.gz";
 
+    if (navigator.userAgent.indexOf("Android") != -1) os_name =
+        "Download", os_link = "download/official_releases.html";
+    if (navigator.userAgent.indexOf("like Mac") != -1) os_name =
+        "Download", os_link = "download/official_releases.html";
+
     button.text = os_name;
     button.href = os_link;
 
