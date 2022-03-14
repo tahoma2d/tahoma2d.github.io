2ebdaf9ebd6d4673d68bef1fc543e12b0dd36e1b
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2022-03-14 23:57:21 +0330

    Make release date update automatically
    + And modify the download section

--- js/detect_os.js
+++ js/detect_os.js
@@ -4,19 +4,50 @@ $(function() {
     var os_name = "Download";
     var os_link = "download/official_releases.html"
 
-    if (navigator.userAgent.indexOf("Win") != -1) os_name =
-        "Download For Windows", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-win.zip";
-    if (navigator.userAgent.indexOf("Mac") != -1) os_name =
-        "Download For macOS", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-osx.dmg";
-    if (navigator.userAgent.indexOf("Linux") != -1) os_name =
-        "Download For Linux", os_link = "https://github.com/tahoma2d/tahoma2d/releases/download/v1.2/Tahoma2D-linux.tar.gz";
+    function fetchData(versionURL) {
+        fetch(versionURL)
+            .then(response => response.json())
+            .then(data => button.href = data.browser_download_url)
+    }
 
-    if (navigator.userAgent.indexOf("Android") != -1) os_name =
-        "Download", os_link = "download/official_releases.html";
-    if (navigator.userAgent.indexOf("like Mac") != -1) os_name =
-        "Download", os_link = "download/official_releases.html";
+    if (navigator.userAgent.indexOf("Win") != -1) {
+        fetchData("https://api.github.com/repos/tahoma2d/tahoma2d/releases/assets/38694525")
+
+        os_name =
+            "Download For Windows";
+
+
+
+    } else if (navigator.userAgent.indexOf("like Mac") != -1) {
+        os_name =
+            "Download";
+        button.href = os_link;
+
+
+
+    } else if (navigator.userAgent.indexOf("Mac") != -1) {
+        fetchData("https://api.github.com/repos/tahoma2d/tahoma2d/releases/assets/36855483")
+
+        os_name =
+            "Download For macOS";
+
+
+
+    } else if (navigator.userAgent.indexOf("Android") != -1) {
+        os_name =
+            "Download";
+        button.href = os_link;
+
+
+
+    } else if (navigator.userAgent.indexOf("Linux") != -1) {
+        fetchData("https://api.github.com/repos/tahoma2d/tahoma2d/releases/assets/36868620")
+
+        os_name =
+            "Download For Linux";
+
+    }
 
     button.text = os_name;
-    button.href = os_link;
 
 });
